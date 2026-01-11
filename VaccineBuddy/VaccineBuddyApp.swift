bb790ec9cdc5a1994ffdacb0014fa05c1011a983
snig-17 <snigdha.17.tiwari@icloud.com>
2026-01-11 20:31:37 +0000

    Initial Commit

---
+++ VaccineBuddy/VaccineBuddyApp.swift
@@ -0,0 +1,17 @@
+//
+//  VaccineBuddyApp.swift
+//  VaccineBuddy
+//
+//  Created by Snigdha Tiwari  on 11/01/2026.
+//
+
+import SwiftUI
+
+@main
+struct VaccineBuddyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
